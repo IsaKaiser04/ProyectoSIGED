2a4183da98442baaaf6114a9142b1ab3b149361d
Manuel Minga <manuel.b.minga@unl.edu.ec>
2026-05-20 21:59:56 -0500

    Realizando las clases del metodo

---
+++ sga/apps/calificaciones/models/Incidencia.py
@@ -0,0 +1,7 @@
+class Incidencia:
+    
+
+    def __init__(self):
+        self.incidencia_tipo = None
+
+        self.calificaciones = []
---
+++ sga/apps/calificaciones/models/evaluacionCriterio.py
@@ -0,0 +1,6 @@
+class EvaluacionCriterio:
+
+    def __init__(self):
+        self.evaluacion_rubrica = None
+
+        self.evaluacion_equivalencias = []
