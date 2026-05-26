8709abe1738de182fb8c95e1334360bc4a8f3b0e
Jossibel <jossibel.perez@unl.edu.ec>
2026-05-26 10:38:03 -0500

    feat(matricula): modulo matricula limpio - eliminado asistencia de esta rama

--- sga/apps/asistencia/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AsistenciaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.asistencia'
-    verbose_name = 'Asistencias'
